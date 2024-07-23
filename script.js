63c8e2164f8d697513d89255114a3599e7bcb7bb
IT21314742 <it21314742@my.sliit.lk>
2024-07-23 22:52:47 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let productImg = document.getElementById("productsImg");
 let btn = document.getElementsByClassName("btn");
 
 btn[0].onclick = function(){
-    productImg.src = "image";
+    productImg.src = "image1.png";
 }
