0e545e320a8bd870ef9533cdcdbfa72bf20459d5
IT21314742 <it21314742@my.sliit.lk>
2024-07-27 22:47:54 +0530

    Update
    Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let productImg = document.getElementById("productsImg");
 let btn = document.getElementsByClassName("btn");
 
 btn[0].onclick = function(){
-    productImg.src = "image1.png";
+    productsImg.src = "image1.png";
 
     for(bt of btn){
         bt.classList.remove("active");
